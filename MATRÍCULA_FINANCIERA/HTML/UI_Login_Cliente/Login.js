4b4f5a07595594f77a4075fb859e2d15be374af7
salas0424 <147124043+salas0424@users.noreply.github.com>
2023-11-23 21:09:28 -0500

    cambios en el loguin js

--- MATRÍCULA_FINANCIERA/HTML/UI_Login_Cliente/Login.js
+++ MATRÍCULA_FINANCIERA/HTML/UI_Login_Cliente/Login.js
@@ -9,3 +9,56 @@ document.addEventListener("change",(e)=>{
         else  $psw.type="text";
         }
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Matriz simulada de usuarios
+    const users = [
+        { username: "1001023456", password: "Mayrelis" },
+        { username: "usuario2", password: "contraseña2" },
+        // Puedes agregar más usuarios según sea necesario
+    ];
+
+    // Obtén referencias a los elementos relevantes
+    const $submitButton = document.getElementById("button");
+    const $passwordInput = document.getElementById('psw');
+    const $idInput = document.getElementById('id');
+    const $visibilityCheckbox = document.getElementById("visible");
+
+    // Agrega un evento al botón de Entrar
+    $submitButton.addEventListener("click", (event) => {
+        event.preventDefault(); // Evita que el formulario se envíe por defecto (puedes ajustarlo según tus necesidades)
+
+        // Simula la autenticación
+        const isAuthenticationSuccessful = authenticateUser($idInput.value, $passwordInput.value);
+
+        if (isAuthenticationSuccessful) {
+            // Redirige al usuario a la página de Facebook después del inicio de sesión exitoso
+            window.location.href = "https://www.facebook.com";
+        } else {
+            // Maneja el caso en que la autenticación falla (puedes mostrar un mensaje de error, etc.)
+            alert("Inicio de sesión fallido. Por favor, verifica tus credenciales.");
+        }
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+    const $passwordInput = document.getElementById('psw');
+    const $mostrarContrasenaCheckbox = document.getElementById("mostrarContrasena");
+
+    $mostrarContrasenaCheckbox.addEventListener("change", function () {
+        if ($mostrarContrasenaCheckbox.checked) {
+            $passwordInput.setAttribute("type", "text");
+        } else {
+            $passwordInput.setAttribute("type", "password");
+        }
+    });
+});
+
+    // Función de autenticación
+    function authenticateUser(username, password) {
+        // Busca el usuario en la matriz
+        const user = users.find(user => user.username === username && user.password === password);
+
+        // Devuelve true si se encuentra el usuario, de lo contrario, devuelve false
+        return !!user;
+    }
+});
